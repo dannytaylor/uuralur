32701a1bf464fd2216b4c7439adfb01ae3c7794d
dt <dhtxhg@gmail.com>
2021-09-18 16:31:23 -0700

    all map aliases

--- data/data.py
+++ data/data.py
@@ -82,17 +82,20 @@ ignore_line_ent = {
 
 
 mapaliases = {
-	'Arena_OutbreakRuin_01.txt':'outbreak',
-	'Arena_Founders_01.txt':'liberty town',
-	'Arena_Praetoria_01.txt':'last bastion', # aka ruined eden
-	'Arena_Industrial_01.txt':'factory',
-	'Arena_Industrial_02.txt':'industrial (new)',
-	'Arena_Outdoor_01.txt':'council earth',
-	'Arena_Outdoor_02.txt':'luna square',
-}
-
-primaryattacks = { # npc costumes used by players throwing off player detection
-	'Blaze',
+	'maps/Arena/Arena_Skyway_01/Arena_Skyway_01.txt':'Skyway',
+	'maps/Arena/Arena_steel_01/Arena_steel_01.txt':'Steel',
+	'maps/Arena/Arena_Outdoor_02/Arena_Outdoor_02.txt':'Luna Square',
+	'maps/Arena/Arena_OutbreakRuin_01/Arena_OutbreakRuin_01.txt':'Outbreak',
+	'maps/Arena/Arena_Praetoria_01/Arena_Praetoria_01.txt':'Last Bastion',
+	'maps/Arena/Arena_Founders_01/Arena_Founders_01.txt':'Liberty Town',
+	'maps/Arena/Arena_Atlas_01/Arena_Atlas_01.txt':'Atlas',
+	'maps/Arena/Arena_Boomtown_01/Arena_Boomtown_01.txt':'Boomtown',
+	'maps/Arena/Arena_Outdoor_01/Arena_Outdoor_01.txt':'Council Earth',
+	'maps/Arena/Arena_Eden_01/Arena_Eden_01.txt':'Eden',
+	'maps/Arena/Arena_Striga_01/Arena_Striga_01.txt':'Striga',
+	'maps/Arena/Arena_Industrial_02/Arena_Industrial_02.txt':'New Industrial',
+	'maps/Arena/Arena_Stadium_01/Arena_Stadium_01.txt':'Stadium',
+	'maps/Arena/Arena_Industrial_02/Arena_Industrial_01.txt':'Seige',
 }
 
 movs = { # npc costumes used by players throwing off player detection
@@ -105,6 +108,5 @@ movs = { # npc costumes used by players throwing off player detection
 
 at_fx = { # determine pset/AT for powers not picked up by actions
 	# FX:[pset,possible_ats,reverse]
-	"WEAPONS/BOW/TRICKARROW/NETARROWHITENERGY.FX": ["Trick Arrow",{"controller","defender/corruptor","mastermind"},True]
 }
 
