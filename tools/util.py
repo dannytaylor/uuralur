112e9389e1c2185c45979014e1449dabcfadb68a
dt <dhtxhg@gmail.com>
2021-11-15 22:38:20 -0800

    minor util updates

--- tools/util.py
+++ tools/util.py
@@ -5,9 +5,14 @@ import base64
 import streamlit as st
 
 @st.cache
-def strsqlquery(table,columns='*',conditions=None):
+def str_sqlq(table,sid=None,mid=None,columns='*',conditions=None):
 	where = ""
-	if conditions: where = " WHERE " + conditions
+	if sid: 
+		where += " WHERE " + "series_id='" + str(sid) 
+		if mid:
+			where += "' AND match_id='" + str(mid) + "'"
+		if conditions:
+			where += conditions
 	return "SELECT " + ','.join(columns) + " FROM " + table + where
 
 
@@ -22,3 +27,11 @@ def image_base64(path):
 		return base64.b64encode(buffer.getvalue()).decode()
 def image_formatter(path):
 	return f'<img src="data:image/jpeg;base64,{image_base64(path)}">'
+
+def set_view_mode(sid,mid):
+	if len(mid) == 1:
+		return 'match'
+	elif len(sid) == 1:
+		return 'series'
+	else:
+		return False
