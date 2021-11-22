449034a9070ea4ede8ab4579cb9ab7eec6fe0450
dt <dhtxhg@gmail.com>
2021-11-22 01:27:18 -0800

    update spike report display, up to datastudio parity nearly

--- match.py
+++ match.py
@@ -1,4 +1,4 @@
-import os, sys, time, math, argparse, json, datetime, yaml, sqlite3, ast
+import os, sys, time, math, argparse, json, datetime, yaml, sqlite3, ast, statistics
 
 import streamlit as st
 ss = st.session_state # global shorthand for this file
@@ -16,7 +16,8 @@ def main(con):
 	# high level score,map,etc. to go hero
 	# match header
 	mh = st.container()
-	mh.header(ss.sid +" > "+str(ss.mid) + " (" + ss.map +")")
+	sid_date = "20" + ss.sid[0:2] + "/" + ss.sid[2:4] + "/" + ss.sid[4:6]
+	mh.header(sid_date +" > Match "+str(ss.mid) + " (" + ss.map +")")
 
 	
 	sqlq = util.str_sqlq('Heroes',ss.sid,ss.mid)
@@ -47,52 +48,70 @@ def main(con):
 	# START SPIKES
 	elif ss.view['match'] == 'spikes':
 		# get spike data for match
-		sqlq = util.str_sqlq('Spikes',ss.sid,ss.mid,columns=['spike_id','time_ms','spike_duration','target','target_team','spike_hp_loss','kill'])
+		sqlq = util.str_sqlq('Spikes',ss.sid,ss.mid)
 		sdf = pd.read_sql_query(sqlq, con)
 		sdf = sdf.rename(columns={"spike_duration": "dur", "spike_id": "#","spike_hp_loss": "dmg","target_team": "team"})
 		sdf['time'] = pd.to_datetime(sdf['time_ms'],unit='ms').dt.strftime('%M:%S')
 		
-
-		# spike summary/header
+		c1,c2,c3,c4,c5,c6,c7 = st.columns([1,1,1,1,1,1,4])
 		teams_dict = {0:{'name':'blu','color':'DodgerBlue'},
 			 1:{'name':'red','color':'Tomato'}}
-		team_sdf = sdf.groupby('team').count()
 
-		c1,c2,c3,c4,c5,c6,c7 = st.columns([1,1,1,1,1,1,4])
-
-
+		# sort spikes by teams
+		sdf['time_m'] = sdf['time_ms']/60000
+		tspikes = {}
+		tkills = {}
 		for t in [0,1]:
+			tspikes[t] = sdf[sdf['team'] == t]
+			tkills[t] = sdf[(sdf['team'] == t) & (sdf['kill'] == 1)]
 			teamstring = """<h2 style="color:{};">{}</h1>""".format(teams_dict[t]['color'],teams_dict[t]['name'],)
 			c1.markdown(teamstring,True)
-			c2.metric("Spikes",1,1)
-			c3.metric("Mean Timing",1,1)
-			c4.metric("Median Timing",1,1)
-			c5.metric("# On Target",1,1)
-			c6.metric("# On Time",1,1)
+	
+		# calc hero timing
+		ht = {0:[],1:[]}
+		h0 = hero_df[(hero_df['team'] == 0)]
+		h1 = hero_df[(hero_df['team'] == 1)]
+		for t in h0['timing']:
+			ht[0] += ast.literal_eval(t)
+		for t in h1['timing']:
+			ht[1] += ast.literal_eval(t)
+
+		for t in [0,1]:
+			t2 = abs(t-1)
+			c2.metric("Spikes",len(tspikes[t2].index),len(tspikes[t2].index)-len(tspikes[t].index))
+
+			ht0 = statistics.mean(ht[t])/1000
+			ht1 = statistics.mean(ht[t2])/1000
+			c3.metric("Mean Timing",round(ht0,2),round(ht0-ht1,3),delta_color='inverse')
+			ht0 = statistics.median(ht[t])/1000
+			ht1 = statistics.median(ht[t2])/1000
+			c4.metric("Median Timing",round(ht0,2),round(ht0-ht1,3),delta_color='inverse')
+
+			a0 = round(tspikes[t]['attacks'].mean(),2)
+			a1 = round(tspikes[t2]['attacks'].mean(),2)
+			c5.metric("Avg Attacks",a0,round(a0-a1,2))
+			a0 = round(tspikes[t]['attackers'].mean(),2)
+			a1 = round(tspikes[t2]['attackers'].mean(),2)
+			c6.metric("Avg Attackers",a0,round(a0-a1,2))
 
 		# graph spikes and kills for summary
 		with c7:
-			sdf['time_m'] = sdf['time_ms']/60000
-			spikes_0 = sdf[sdf['team'] == 0]
-			kills_0 = sdf[(sdf['team'] == 0) & (sdf['kill'] == 1)]
-			spikes_1 = sdf[sdf['team'] == 1]
-			kills_1 = sdf[(sdf['team'] == 1) & (sdf['kill'] == 1)]
 			fig = go.Figure()
 			# spike traces
 			lineoffset=50
 			fig.add_trace(go.Line(
-				x=spikes_1['time_m'],
+				x=tspikes[1]['time_m'],
 				name='spikes',
-				y=spikes_1['#']+lineoffset,
-				text=spikes_1['target'],
+				y=tspikes[1]['#']+lineoffset,
+				text=tspikes[1]['target'],
 				mode='lines',
 				line=dict(color='DodgerBlue', width=6),
 				hovertemplate = "%{x:.2f}<br><b>%{text}</b><br>",
 			))
 			fig.add_trace(go.Line(
-				x=spikes_0['time_m'],
-				y=spikes_0['#'],
-				text=spikes_0['target'],
+				x=tspikes[0]['time_m'],
+				y=tspikes[0]['#'],
+				text=tspikes[0]['target'],
 				name='spikes',
 				mode='lines',
 				line=dict(color='tomato', width=6),
@@ -101,34 +120,34 @@ def main(con):
 
 			# kill traces
 			fig.add_trace(go.Scatter(
-				x=kills_0['time_m'],
-				y=kills_0['#'],
-				text=spikes_0['target'],
+				x=tkills[0]['time_m'],
+				y=tkills[0]['#'],
+				text=tspikes[0]['target'],
 				name='kills',
 				mode='markers',
-				marker_color='tomato',
-				marker=dict(size=12,line=dict(width=2,color='DarkSlateGrey')),
+				marker_color='white',
+				marker=dict(size=12,line=dict(width=4,color='tomato')),
 				hovertemplate = "%{x:.2f}<br><b>%{text}</b><br>kill",
 			))
 			fig.add_trace(go.Scatter(
-				x=kills_1['time_m'],
-				y=kills_1['#']+lineoffset,
-				text=spikes_1['target'],
+				x=tkills[1]['time_m'],
+				y=tkills[1]['#']+lineoffset,
+				text=tspikes[1]['target'],
 				name='kills',
 				mode='markers',
-				marker_color='DodgerBlue',
-				marker=dict(size=12,line=dict(width=2,color='DarkSlateGrey')),
+				marker_color='white',
+				marker=dict(size=12,line=dict(width=4,color='DodgerBlue')),
 				hovertemplate = "%{x:.2f}<br><b>%{text}</b><br>kill",
 			))
-
-
+			
 			fig.update_layout(
 				showlegend=False,
 				height=240,
 				xaxis_title="match time (m)",
 				xaxis={'range':[0,10]},
 				yaxis={'showticklabels':False,'title':'# spikes'},
-				margin={'t': 10,'b':0,'l':56,'r':0},
+				margin={'t': 0,'b':0,'l':52,'r':0},
+				plot_bgcolor='rgba(0,0,0,0)',
 			)
 			st.plotly_chart(fig,use_container_width=True,config={'displayModeBar': False})
 
@@ -138,29 +157,32 @@ def main(con):
 		# left side
 		with c1:
 			# st.subheader('spike list')
+			colourmap = {0:'DodgerBlue',1:'Tomato'}
+
+			# spikes counts by players
+			pc0 = sdf[(sdf['team'] == 0)]
+			pc1 = sdf[(sdf['team'] == 1)]
+			pc0 = pc0.groupby('target').count().sort_values('#',ascending=False)
+			pc1 = pc1.groupby('target').count().sort_values('#',ascending=False)
+			pc0['colour'] = 'DodgerBlue'
+			pc1['colour'] = 'Tomato'
+			pc = pd.concat([pc0,pc1])
+			pc = pc.sort_values('#',ascending=False)
 			
-			player_sum = sdf.groupby('target').sum().sort_values('#',ascending=False)
-			player_count = sdf.groupby('target').count().sort_values('#',ascending=False)
-			player_color = []
-			for p in player_sum['team']:
-				if p == 0:
-					player_color.append('DodgerBlue')
-				else:
-					player_color.append('Tomato')
 
 			fig = go.Figure()
 			fig.add_trace(go.Bar(
-				x=player_count.index,
-				y=player_count['#'],
+				x=pc.index,
+				y=pc['#'],
 				name='targets',
-				marker_color=player_color,
+				marker_color=pc['colour'],
 				opacity=0.5
 			))
 			fig.add_trace(go.Bar(
-				x=player_count.index,
-				y=player_count['kill'],
+				x=pc.index,
+				y=pc['kill'],
 				name='deaths',
-				marker_color=player_color
+				marker_color=pc['colour'],
 			))
 			fig.update_layout(
 				barmode="overlay",
@@ -212,9 +234,10 @@ def main(con):
 			sf['dmg'] = sf['dmg']
 
 
-			sf_write = sf[['#','time','team','kill','target','dur','dmg']]
+			sf_write = sf[['#','time','team','kill','target','dur','attacks','attackers','greens','dmg']]
 			gb = GridOptionsBuilder.from_dataframe(sf_write)
 			gb.configure_columns(['#','team','kill'],width=22)
+			gb.configure_columns(['attacks','attackers','greens'],width=48)
 			gb.configure_columns(['time','dur','dmg'],width=64)
 			gb.configure_selection('single', pre_selected_rows=[0])
 			gb.configure_columns('dur',type='customNumericFormat',precision=1)
@@ -285,7 +308,7 @@ def main(con):
 				y=hp_df['hp_loss'],
 				name='hp loss',
 				mode='lines',
-				line=dict(color='SlateBlue', width=6),
+				line=dict(color='SlateBlue', width=6,dash='dash'),
 			))
 			hp_time = hp_df['spike_time'].tolist()
 
@@ -315,15 +338,30 @@ def main(con):
 					acolours.append('limegreen')
 				elif 'Teleport' in tags:
 					acolours.append('gold')
+				elif 'Phase' in tags:
+					acolours.append('DeepSkyBlue')
 				else:
 					acolours.append('Grey')
 
 
+			hp_range=[min(0,hp_time[0]),max(2,hp_time[-1])]
+			# hp graph, data above
+			hp_fig.update_layout(
+				height=270,
+				showlegend=False,
+				xaxis_title="spike time (s)",
+				yaxis={'visible': True, 'title':'hp','fixedrange':True,'range':[0,max(2000,max(hp_df['hp_loss']))]},
+				xaxis={'visible':False,'fixedrange':True,'range':hp_range},
+				margin={'t': 20,'b':0,'l':0,'r':0},
+				hovermode="x unified"
+			)
+			st.plotly_chart(hp_fig, use_container_width=True,config={'displayModeBar': False})
 
 			# add action markers to HP graph
-			hp_fig.add_trace(go.Scatter(
+			act_fig = go.Figure()
+			act_fig.add_trace(go.Scatter(
 				x=sl['cast'],
-				y=[1100]*len(sl['cast']),
+				y=[1.6]*len(sl['cast']),
 				name='',
 				text=sl['actor']+"<br>"+sl['action'],
 				marker_color=acolours,
@@ -331,27 +369,27 @@ def main(con):
 				mode='markers',
 				hovertemplate = "<b>cast time</b> <br>%{text}"
 			))
-			hp_fig.add_trace(go.Scatter(
+			act_fig.add_trace(go.Scatter(
 				x=sl['hit'],
-				y=[900]*len(sl['cast']),
+				y=[0.6]*len(sl['cast']),
 				name='',
 				text=sl['actor']+"<br>"+sl['action'],
 				marker_color=acolours,
 				marker=dict(size=12,line=dict(width=2,color='DarkSlateGrey')),
-				opacity=0.5,
+				# opacity=0.5,
 				mode='markers',
 				hovertemplate = "<b>hit time</b> <br>%{text}"
 			))
-			hp_fig.update_layout(
-				height=300,
+			act_fig.update_layout(
+				height=91,
 				showlegend=False,
 				xaxis_title="spike time (s)",
-				yaxis={'visible': True, 'title':'hp','fixedrange':True,'range':[-10,max(2000,max(hp_df['hp_loss']))]},
-				xaxis={'visible':True,'fixedrange':True,'range':[min(0,hp_time[0]),max(2,hp_time[-1])]},
-				margin={'t': 20,'b':0,'l':0,'r':0},
-				hovermode="x unified"
+				yaxis={'visible': True,'title':'hit/cast','showticklabels':False,'fixedrange':True,'range':[0,2]},
+				xaxis={'visible':True,'fixedrange':True,'range':hp_range},
+				margin={'t': 0,'b':40,'l':48,'r':0},
+				plot_bgcolor='rgba(0,0,0,0)',
 			)
-			st.plotly_chart(hp_fig, use_container_width=True,config={'displayModeBar': False})
+			st.plotly_chart(act_fig, use_container_width=True,config={'displayModeBar': False})
 
 
 			# render html text as html
