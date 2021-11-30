1cbb80c7475fad8a1cafda809f58047e7c94aeaa
dt <dhtxhg@gmail.com>
2021-11-29 16:54:28 -0800

    file check valid

--- view/upload.py
+++ view/upload.py
@@ -1,4 +1,4 @@
-import os,datetime,yaml,hashlib
+import os,datetime,yaml,hashlib,fnmatch
 import streamlit as st
 ss = st.session_state # global shorthand for this file
 import parse
@@ -32,9 +32,9 @@ def main():
 		Demos of the same match from multiple pov's will not be flagged as duplicate files.	
 		File names are not preserved when uploading. 
 
-		There's a daily limit to uploads, so don't mass-upload demos or re-upload files.
+		There is a daily limit to number of uploads to manage storage space, so don't mass-upload demos.
 		Contact Xhiggy if you'd like to add a large amount of demos to the site.
-		Invalid files will likely result in an error (team sizes too small, more than 2 teams, etc.)
+		Invalid demos will likely result in an error or incorrect parsed data (team sizes too small, more than 2 teams, etc.)
 
 		""")
 
@@ -48,67 +48,73 @@ def main():
 		# f_name = uploaded_file.name
 		bytes_data = uploaded_file.getvalue()
 
-		f_path = "publicdemos/" + f_sid +"/"
-		parsepath = f_path
-		parseflag = 'm' # parse as match if series exists in db
-		if not os.path.exists(f_path): # if new folder/series
-			os.makedirs(f_path)
-			parseflag = 's'
-		folder_size = len(os.listdir(f_path))# number of files in folder
-		if folder_size < config['daily_upload_limit']: # so storage can't get overrun
-			
-			# save upload
-			list_fnames = [int(x.replace('.cohdemo','')) for x in os.listdir(f_path)]
-			if list_fnames:
-				f_mid = str(max(list_fnames)+1)
-			else:
-				f_mid = 1
-			f_name = str(f_mid) + ".cohdemo" # demo name by number in folder
-
-			sid = f_sid
-			mid = f_mid
-
-			def go_to_match():
-				ss.view = {'match':'summary'}
-				ss.sid = sid
-				ss.mid = mid
-				ss.new_mid = True
-
-				ss['app_choice'] = 'players'
-
-				params = st.experimental_get_query_params()
-				params['s'] = sid
-				params['m'] = mid
-				st.experimental_set_query_params(**params) 
-
-
-			with st.spinner('checking for duplicates of file'):
-				dup_check = is_file_dupl(f_path,bytes_data)
-			if dup_check:
-				mid = dup_check.replace('.cohdemo','')
-				print(sid,mid)
-				st.warning('Demo appears to already exist in uploads')
-				st.button('click here to view',on_click=go_to_match)
-			else:
-				f = open(f_path+f_name, "wb")
-				f.write(bytes_data)
-				f.close()
-				print("{} saved".format(uploaded_file))
-				# os.system("parse.py -m {} -d publicdemos.db".format(f_path + f_name))
-				with st.spinner('attemping to read demo...'):
-					try:
-						if parseflag == 'm':
-							parsepath += f_name
-						parse.main(['-'+parseflag,parsepath])
-
-						st.success('Demo parsed successfully')
-						st.button('click here to view',on_click=go_to_match)
-						st.caption('(remember the link to access)')
-					# init_series()
-					# init_matches()
-					except:
-						st.error('Problem reading demo file.')
-						os.remove(f_path+f_name)
-
+		demo_matches = ['Version','maps/Arena','Time']
+		check_valid  = all(match in str(bytes_data[0:500]) for match in demo_matches)
+		if not check_valid:
+			st.error('Demo cannot be read by parser')
 		else:
-			st.error('Uploads over limit for the day. Try again later.')
+
+			f_path = "publicdemos/" + f_sid +"/"
+			parsepath = f_path
+			parseflag = 'm' # parse as match if series exists in db
+			if not os.path.exists(f_path): # if new folder/series
+				os.makedirs(f_path)
+				parseflag = 's'
+			folder_size = len(os.listdir(f_path))# number of files in folder
+			if folder_size < config['daily_upload_limit']: # so storage can't get overrun
+				
+				# save upload
+				list_fnames = [x.replace('.cohdemo','') for x in os.listdir(f_path)]
+				list_fnames = [int(x.replace('.cohdemo','')) for x in list_fnames if x.isnumeric()]
+				if list_fnames:
+					f_mid = str(max(list_fnames)+1)
+				else:
+					f_mid = 1
+				f_name = str(f_mid) + ".cohdemo" # demo name by number in folder
+
+				sid = f_sid
+				mid = f_mid
+
+				def go_to_match():
+					ss.view = {'match':'summary'}
+					ss.sid = sid
+					ss.mid = mid
+					ss.new_mid = True
+
+					ss['app_choice'] = 'players'
+
+					params = st.experimental_get_query_params()
+					params['s'] = sid
+					params['m'] = mid
+					st.experimental_set_query_params(**params) 
+
+
+				with st.spinner('checking for duplicates of file'):
+					dup_check = is_file_dupl(f_path,bytes_data)
+				if dup_check:
+					mid = dup_check.replace('.cohdemo','')
+					st.warning('Demo appears to already exist in uploads')
+					st.button('click here to view',on_click=go_to_match)
+				else:
+					f = open(f_path+f_name, "wb")
+					f.write(bytes_data)
+					f.close()
+					print("{} saved".format(uploaded_file))
+					# os.system("parse.py -m {} -d publicdemos.db".format(f_path + f_name))
+					with st.spinner('attemping to read demo...'):
+						try:
+							if parseflag == 'm':
+								parsepath += f_name
+							parse.main(['-'+parseflag,parsepath])
+
+							st.success('Demo parsed successfully')
+							st.button('click here to view',on_click=go_to_match)
+							st.caption('(remember the link to access)')
+						# init_series()
+						# init_matches()
+						except:
+							st.error('Problem reading demo file.')
+							os.remove(f_path+f_name)
+
+			else:
+				st.error('Uploads over limit for the day. Try again later.')
